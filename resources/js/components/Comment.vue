3aaba480049fd3d53860bd8da2f1ae5c5cc5be92
James <james@jastewart.co.uk>
2022-05-19 13:24:18 +0100

    comment

--- resources/js/components/Comment.vue
+++ resources/js/components/Comment.vue
@@ -29,7 +29,7 @@ defineProps({
                     text-white text-xl
                 "
             >
-                {{comment.author_initials}}
+                {{ comment.author_initials }}
             </div>
         </div>
         <div
@@ -49,7 +49,6 @@ defineProps({
             </span>
             <p class="text-sm">
                 <SanitisedHtml :html-string="comment.message" />
-                <!-- {{ comment.message }} -->
             </p>
             <div class="mt-4 flex items-center">
                 <div class="flex -space-x-2 mr-2">
@@ -73,28 +72,33 @@ defineProps({
                             ? comment.child_comments.length
                             : 0
                     }}
-                    {{ comment.child_comments ? "reply" : "replies" }}
+                    <span v-if="comment.child_comments">
+                        {{
+                            comment.child_comments.length === 1
+                                ? "reply"
+                                : "replies"
+                        }}
+                    </span>
                 </div>
             </div>
 
             <div v-if="showChildReplies" class="space-y-4">
+                <h4
+                    class="
+                        my-5
+                        uppercase
+                        tracking-wide
+                        text-gray-400
+                        font-bold
+                        text-xs
+                    "
+                >
+                    Replies
+                </h4>
                 <div
                     v-for="childComment in comment.child_comments"
                     :key="childComment.id"
                 >
-                    <h4
-                        class="
-                            my-5
-                            uppercase
-                            tracking-wide
-                            text-gray-400
-                            font-bold
-                            text-xs
-                        "
-                    >
-                        Replies
-                    </h4>
-
                     <div class="flex">
                         <div class="flex-shrink-0 mr-3">
                             <div
@@ -113,7 +117,7 @@ defineProps({
                                     text-white text-xl
                                 "
                             >
-                            {{childComment.author_initials}}
+                                {{ childComment.author_initials }}
                             </div>
                         </div>
                         <div
@@ -132,41 +136,13 @@ defineProps({
                                 {{ formatDateTime(childComment.created_at) }}
                             </span>
                             <p class="text-sm">
-                                {{ childComment.message }}
+                                <SanitisedHtml
+                                    :html-string="childComment.message"
+                                />
                             </p>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="flex">
-                        <div class="flex-shrink-0 mr-3">
-                            <img
-                                class="mt-3 rounded-full w-6 h-6 sm:w-8 sm:h-8"
-                                src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80"
-                                alt=""
-                            />
-                        </div>
-                        <div
-                            class="
-                                flex-1
-                                bg-gray-100
-                                rounded-lg
-                                px-4
-                                py-2
-                                sm:px-6 sm:py-4
-                                leading-relaxed
-                            "
-                        >
-                            <strong>Sarah</strong>
-                            <span class="text-xs text-gray-400">3:34 PM</span>
-                            <p class="text-xs sm:text-sm">
-                                Lorem ipsum dolor sit amet, consetetur
-                                sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam
-                                erat, sed diam voluptua.
-                            </p>
-                        </div>
-                    </div>
-                </div> -->
             </div>
         </div>
     </div>

4449228f2c9b6bfc3fcd9ef4dafb290558a739b4
James <james@jastewart.co.uk>
2022-06-26 20:04:10 +0100

    Create word locations table

---
+++ database/migrations/2022_06_26_190145_create_word_locations_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordLocationsTable extends Migration
+{
+    private const TABLE = 'word_locations';
+
+    public function up()
+    {
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('word_id');
+            $table->unsignedBigInteger('location_id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+}

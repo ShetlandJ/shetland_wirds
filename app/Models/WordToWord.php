ae05f4491526f484ad2eebbdb9835f7bdce9ea71
James <james@jastewart.co.uk>
2022-07-09 16:16:55 +0100

    Update composer & add mogration

--- app/Models/WordToWord.php
+++ app/Models/WordToWord.php
@@ -4,18 +4,19 @@ namespace App\Models;
 
 use App\Models\Word;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WordToWord extends Model
 {
     protected $table = 'words_to_words';
 
-    public function word()
+    public function word(): BelongsTo
     {
-        return $this->belongsTo(Word::class, 'parent_word_id');
+        return $this->belongsTo(Word::class, 'word_d');
     }
 
-    public function wordChild()
+    public function wordChild(): BelongsTo
     {
-        return $this->belongsTo(Word::class, 'word_id');
+        return $this->belongsTo(Word::class, 'linked_word_id');
     }
 }

1a8a56083aa0f99e7505fe721a35c1b740aa11a6
James <james@jastewart.co.uk>
2022-07-15 16:09:48 +0100

    Add.update your entries

--- resources/js/components/locations/Locations.vue
+++ resources/js/components/locations/Locations.vue
@@ -13,18 +13,27 @@ onMounted(() => {
     if (userSelectedLocations.length > 0) {
         showAddForm.value = false;
     }
-})
+});
 </script>
 
 <template>
     <div v-if="word">
         <p class="mb-2 dark:text-white" v-if="isLoggedIn">
-            Where in Shetland is this word spoken? <span v-if="showAddForm">Add your own below</span>
-        </p>
+            Where in Shetland is this word spoken?</p>
 
-        <div v-if="!showAddForm && userSelectedLocations.length > 0" class="dark:text-white">
-            You have previously selected locations for <b>{{ word.word }}</b>. If you
-            would like to amend your selection, please click
+        <p class="mb-2 dark:text-white">
+            We are gathering data and
+            will display a heatmap when we receive enough information to make
+            the data useful!
+        </p>
+        <p class="mt-4" v-if="showAddForm">{{userSelectedLocations.length > 0 ? 'Edit your entries:' : 'Add your own below:'}}</p>
+
+        <div
+            v-if="!showAddForm && userSelectedLocations.length > 0"
+            class="dark:text-white"
+        >
+            You have previously selected locations for <b>{{ word.word }}</b
+            >. If you would like to amend your selection, please click
             <button class="underline" @click="toggleShowAddForm">here</button>.
         </div>
 

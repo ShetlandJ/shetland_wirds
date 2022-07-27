ff5fa9f271f95a497c0aa9cc0aa8da2c4aff3f56
James <electionnightspecial@gmail.com>
2022-07-27 08:14:55 +0100

    Update i18n.js

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -3,7 +3,7 @@ import en from './langs/en.json';
 import es from './langs/es.json';
 import fr from './langs/fr.json';
 import no from './langs/no.json';
-import shet from './Lang's/shet.json'
+import shet from './langs/shet.json'
 
 import { createI18n } from "vue-i18n";
 

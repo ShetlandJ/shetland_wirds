0add28bd85c1c3050ed63a9228cd80a2d8a87324
James <james@jastewart.co.uk>
2022-07-29 16:43:14 +0100

    fix broken

--- resources/js/components/comments/ChildComment.vue
+++ resources/js/components/comments/ChildComment.vue
@@ -70,7 +70,7 @@ const toggleEdit = () => {
                 class="mt-4"
                 :comment-id="childComment.id"
                 :word="word"
-                :action-message="t('word.comment.update)"
+                :action-message="t('word.comments.update')"
                 :edit-mode="editMode"
                 @editing-complete="editMode = false"
             />

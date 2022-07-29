63c62320387a885be68d5512061a9332d2a3afe9
James <james@jastewart.co.uk>
2022-07-29 13:29:05 +0100

    fix!!!

--- app/Services/WordService.php
+++ app/Services/WordService.php
@@ -442,7 +442,7 @@ class WordService
 
     public function getTopViewedPages(): array
     {
-        $userLogs = UserLog::where('created_at', '>=', now()->subDays(30))->get()
+        $userLogs = UserLog::where('created_at', '>=', now()->subDays(30))
             ->join('words', 'words.id', '=', 'user_logs.word_id')
             ->get();
 

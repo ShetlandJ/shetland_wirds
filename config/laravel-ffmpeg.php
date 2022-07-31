56cd88e467e8b6a1247a387f4c881c4745658f0b
James <james@jastewart.co.uk>
2022-07-31 14:09:03 +0100

    binaries

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -1,23 +1,23 @@
 <?php
 
 return [
-    'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', storage_path() . '/ffmpeg'),
+    // 'ffmpeg' => [
+    //     'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
 
-        'threads' => 12,   // set to false to disable the default 'threads' filter
-    ],
+    //     'threads' => 12,   // set to false to disable the default 'threads' filter
+    // ],
 
-    'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', storage_path() . '/ffprobe'),
-    ],
+    // 'ffprobe' => [
+    //     'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
+    // ],
+    'ffmpeg.binaries' => '/usr/bin/ffmpeg',
+    'ffprobe.binaries' => '/usr/bin/ffprobe',
 
     'timeout' => 3600,
 
     'enable_logging' => true,
-    'log_channel' => env('LOG_CHANNEL', 'stack'),   // set to false to completely disable logging
 
     'set_command_and_error_output_on_exception' => false,
 
     'temporary_files_root' => env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir()),
-    'temporary_files_encrypted_hls' => env('FFMPEG_TEMPORARY_ENCRYPTED_HLS', env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir())),
 ];

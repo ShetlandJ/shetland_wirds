6990627bc1cfc5955fea41d2b33b462c45ac164e
James <james@jastewart.co.uk>
2022-07-31 16:21:05 +0100

    try t ry again

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,13 +2,13 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'storage/ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
 
         'threads' => 12,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'storage/ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
     ],
 
     'timeout' => 3600,

49bd432d86833071e123aa2f48018e0a90c10997
James <james@jastewart.co.uk>
2022-07-31 13:19:08 +0100

    adapt ffmpg

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -14,8 +14,10 @@ return [
     'timeout' => 3600,
 
     'enable_logging' => true,
+    'log_channel' => env('LOG_CHANNEL', 'stack'),   // set to false to completely disable logging
 
     'set_command_and_error_output_on_exception' => false,
 
     'temporary_files_root' => env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir()),
+    'temporary_files_encrypted_hls' => env('FFMPEG_TEMPORARY_ENCRYPTED_HLS', env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir())),
 ];

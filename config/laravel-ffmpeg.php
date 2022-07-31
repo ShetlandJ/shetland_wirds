d3bbe14c0573518906f8459c61c013b9598659e3
James <james@jastewart.co.uk>
2022-07-31 14:34:58 +0100

    ffmpeg

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,20 +2,20 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'usr/local/bin/ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
 
         'threads' => 12,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'usr/local/bin/ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
     ],
 
     'timeout' => 3600,
 
-    'enable_logging' => true,
-
-    'set_command_and_error_output_on_exception' => false,
+    'log_channel' => env('LOG_CHANNEL', 'stack'),   // set to false to completely disable logging
 
     'temporary_files_root' => env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir()),
+
+    'temporary_files_encrypted_hls' => env('FFMPEG_TEMPORARY_ENCRYPTED_HLS', env('FFMPEG_TEMPORARY_FILES_ROOT', sys_get_temp_dir())),
 ];

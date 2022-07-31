aa14bade6b9759934cc89b4dd777defe3f65cce4
James <james@jastewart.co.uk>
2022-07-31 13:25:51 +0100

    hardcode binaries

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,13 +2,13 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', 'usr/bin/ffmpeg'),
 
         'threads' => 12,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', 'usr/bin/ffprobe'),
     ],
 
     'timeout' => 3600,
